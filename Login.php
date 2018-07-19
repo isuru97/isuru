c1413c943f4c9dfeb704361a93be5a085220b063
Nimantha <isurunimantha15@gmail.com>
2018-07-19 21:58:26 +0530

    first attempt

---
+++ Login.php
@@ -0,0 +1,59 @@
+<html>
+	<head>
+		<title>Login</title>
+			<script type="text/javascript" src="js/Javascript.js"></script>
+			<link rel="stylesheet" type="text/css" href="Style.css">
+			<link rel="stylesheet" href="2.css" type="text/css" media="screen">
+			<link rel="stylesheet" href="5.css" type="text/css" media="screen">
+	</head>
+	<body>
+		
+		<header><!-- Defining the header-->
+		<div class="header">
+			<font type ="sans-serif" size="6" color="#1a0000"> E-PARTY|</font>
+			<font type ="sans-serif" size="4" color="##1a0000"> LET'S PLAN YOUR PARTY ONLINE :)</font>
+			<div class="social_icons">
+				<a href="#" title="Facebook"><img alt="" src="2.jpg" width="36px" height="40px"></a>
+				<a href="#" title="Twitter"><img alt="" src="3.jpg"width="36px" height="40px"></a>
+				<a href="#" title="gmail"><img alt="" src="4.jpg"width="36px" height="40px"></a>
+			</div>
+			<div class="search">
+				<form action="" method="get">
+					<input type="text" name="" value="" />
+					<input type="submit" value="search" />
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+   		</header>
+   		
+		<div class="bg-img">
+			<form name="login">
+			
+			<div class="logo">
+				<a href="" title="LOGO"><img src="logo.png"></a>
+    		
+				
+					<div class="container">
+						<b>Email :</b>
+						<input type="text" name="email" >
+						<br><br><br>
+						<b>Password :</b>
+						<input type="text" name="password">
+						<br><br><br>
+					
+					
+					<button type="button" name="submit" class="btn1" >Submit</button>
+					<button type="button" name="reset" class="btn2">Reset</button>
+					<p>Not registered? <a href = "Register.php">SignUp </a></p>
+					</div>
+				</form>	
+			</div>
+		</div>
+		<div class="footer"><!-- Defining the footer section of the page -->
+		 E-party. All rights reserved &copy;   &nbsp;:&nbsp; <a class="link" href="#">Contact US</a>
+    	</div>
+	</body>
+</html>
+
+		
